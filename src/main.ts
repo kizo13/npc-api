940deced12a088718276efde34cbd49860377f05
zkilvinger <zkilvinger@precognox.com>
2021-01-20 09:00:01 +0100

    init app

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import * as compression from 'compression';
+import * as cookieParser from 'cookie-parser';
+import * as helmet from 'helmet';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = Number(configService.get('PORT'));
+  app.setGlobalPrefix('api/v1');
+  app.use(helmet());
+  app.use(compression());
+  app.use(cookieParser());
+
+  await app.listen(port, async () => {
+    const url = await app.getUrl();
+    const address = `http://${url}:${port}/`;
+    Logger.log(`Listening at ${address}`);
+  });
+}
+bootstrap();

76b50eb6eabff1350d0cc3d5680bcb3a2af1c37a
zkilvinger <zkilvinger@precognox.com>
2021-01-30 08:39:50 +0100

    add file service to check mime type

---
+++ src/_shared/services/file.service.ts
@@ -0,0 +1,41 @@
+import {
+  Injectable,
+  InternalServerErrorException,
+  UnsupportedMediaTypeException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as mmm from 'mmmagic';
+import FileInvalidException from '../exceptions/file-invalid.exception';
+
+@Injectable()
+class FileService {
+  private readonly magic;
+
+  constructor(private readonly configService: ConfigService) {
+    this.magic = new mmm.Magic(mmm.MAGIC_MIME_TYPE);
+  }
+
+  async checkMimeType(file) {
+    let mimeType;
+    try {
+      mimeType = await this.getMimeType(file);
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+    if (!this.configService.get('SUPPORTED_FILE_MIME_TYPES').includes(mimeType))
+      throw new UnsupportedMediaTypeException();
+    if (!file) {
+      throw new FileInvalidException();
+    }
+  }
+
+  private getMimeType(file) {
+    return new Promise((resolve, reject) => {
+      this.magic.detect(file.buffer, (err, mimeType) =>
+        err ? reject(err) : resolve(mimeType),
+      );
+    });
+  }
+}
+
+export default FileService;
